abe9ca5637c914c7f80c1690f2695cbfba46906d
samsonkassa-dev <samsonkassa25@gmail.com>
2024-02-20 12:02:23 +0300

    First commit - letter added

---
+++ src/app/page.js
@@ -0,0 +1,8 @@
+import Image from "next/image";
+import page from "./landing/page";
+import Page from "./landing/page";
+import * as THREE from "three";
+
+export default function Home() {
+  return <Page />;
+}
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      gradientColorStops: {
+        "gradient-start": "#ff00ff", // Replace with your start color
+        "gradient-end": "#00ffff", // Replace with your end color
+      },
+      backgroundGradientColors: ["gradient-start", "gradient-end"],
+    },
+  },
+  plugins: [],
+};
