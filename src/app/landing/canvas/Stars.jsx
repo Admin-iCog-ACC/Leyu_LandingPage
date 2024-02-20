bf92ce22b1f723141c4f14a0f7d4662f190f4b0c
abrsh-10 <abrhamsisay33@gmail.com>
2024-02-20 14:26:34 +0300

    background letters added

--- src/app/landing/canvas/Stars.jsx
+++ src/app/landing/canvas/Stars.jsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState, useRef, Suspense } from "react";
+import { useState, useRef,useEffect, Suspense } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { Points, PointMaterial, Preload } from "@react-three/drei";
 import * as random from "maath/random/dist/maath-random.esm";
@@ -8,25 +8,39 @@ import { TextureLoader } from 'three';
 
 const Stars = (props) => {
   const ref = useRef();
-  
-  const [sphere] = useState(() => random.inSphere(new Float32Array(5000), { radius: 1.2 }));
-  const texture = new TextureLoader().load('/the.png'); 
+
+  const [sphere] = useState(() => random.inSphere(new Float32Array(1000), { radius: 1.2 }));
+
+  const [randomNumber, setRandomNumber] = useState(1);
+
+  const letterChoser = () => {
+    const texture = new TextureLoader().load(`/letter${randomNumber}.png`);
+    return texture;
+  };
 
   useFrame((state, delta) => {
     ref.current.rotation.x -= delta / 10;
     ref.current.rotation.y -= delta / 15;
   });
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const newRandomNumber = Math.floor(Math.random() * 7) + 1;
+      setRandomNumber(newRandomNumber);
+    }, 2000); // Change the letter every 2 seconds (adjust as needed)
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <group rotation={[0, 0, Math.PI / 4]}>
       <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
         <PointMaterial
           transparent
-          color='#f272c8'
-          size={0.01}
-          sizeAttenuation={true}
-          depthWrite={false}
-          map={texture}
+          opacity={1}
+          size={0.04}
+          color="#02C27D"
+          map={letterChoser()}
         />
       </Points>
     </group>
@@ -35,7 +49,7 @@ const Stars = (props) => {
 
 const StarsCanvas = () => {
   return (
-    <div className='min-h-screen flex items-center justify-center absolute'>
+    <div className='min-h-screen flex items-center justify-center absolute -z-10 bg-black'>
       <div className=' w-screen h-screen'>
         <Canvas camera={{ position: [0, 0, 0] }}>
           <Suspense fallback={null}>
