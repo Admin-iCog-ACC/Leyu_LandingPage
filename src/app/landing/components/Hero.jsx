d9de160dddacbb0471690156b9d5bbb049f17938
samsonkassa-dev <samsonkassa25@gmail.com>
2024-02-22 11:41:49 +0300

    gradient edits

--- src/app/landing/components/Hero.jsx
+++ src/app/landing/components/Hero.jsx
@@ -1,19 +1,22 @@
 import React from "react";
-import { Lato } from "next/font/google";
-
-const lato = Lato({ subsets: ['latin'], 
-weight: ['100', '300', '400', '700', '900']})
-
 
 const Hero = () => {
   return (
-    <div className={lato.className}>
-      <div className="flex justify-center font-bold items-center py-40 w-full max-w-600px">
-        <h1 className="herotext-gradient text-[70px] font-extrabold line uppercase">
+    <div className="relative flex items-center justify-center">
+    <div className="absolute left-0 w-[13%] h-1/4" style={{backgroundImage: 'radial-gradient(circle, rgba(47, 129, 174, 0.55))'}} />
+    <div className="absolute right-0 w-[13%] h-1/4" style={{backgroundImage: 'radial-gradient(circle, rgba(92, 62, 188, 0.55), transparent)'}} />
+    <div className={`z-10 `}>
+      <div className="flex flex-col justify-center font-bold items-center py-40 w-full max-w-600px">
+        <h1 className="herotext-gradient text-[70px] font-extrabold uppercase">
           Crowdsourcing datasets
         </h1>
+        <h2 className="pt-5 text-white text-[36px] text-center font-normal">
+          for low-resource languages across Africa <br/> Fueling innovation tailored
+          to local challenges/needs!
+        </h2>
       </div>
     </div>
+  </div>
   );
 };
 
